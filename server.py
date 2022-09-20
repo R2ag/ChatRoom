13cefa576144b7bca8db1ca7e64d71cd65f48657
Rafael <rlag@outlook.com>
2022-09-20 02:44:28 -0300

    new server

--- server.py
+++ server.py
@@ -1,84 +1,115 @@
 import socket
-import _thread
+import threading
 import json
+import queue
+
 
 PORT = 5000  # Porta que o Servidor esta
-LISTA_USUARIO = []
+USERS = []
+MESSAGES = queue.Queue()
+CHAT = queue.Queue()
+
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp.bind("", PORT)
+
+def receive():
+    while True:
+        try:
+            msg, client = udp.recvfrom(1024)
+            MESSAGES.put((msg, client))
+        except:
+            pass
+
+def controll():
+    while True:
+        while not MESSAGES.empty():
+            msg, client = MESSAGES.get()
+            msg_json =  msg.decode('utf-8')
+            msg_dic = json.loads(msg_json)
+            
+            if msg_dic["acao"] == 1:
+                entrar_sala(msg_dic, client)
+            elif msg_dic["acao"] == 2:
+                sair_sala(msg_dic, client)
+            elif msg_dic["acao"] == 3:
+                chat_room(msg_dic, client)
+            
+
+
+def entrar_sala(msg_dic, client):
+    adicionar_usuario(msg_dic, client)
+    msg = {
+        "acao": 1,
+        "nome": msg_dic["nome"],
+        "id_sala": msg_dic["id_sala"],
+        "status": 1
+    }
+    msg_json = json.dumps(msg)
+    udp.sendto(msg_json.encode("utf-8"), client)
+
 
 def adicionar_usuario(usuario, cliente):
     novo_usuario = {}
     novo_usuario["nome"] = usuario["nome"]
     novo_usuario["conexao"] = cliente
     novo_usuario["id_sala"] = usuario["id_sala"]
-    LISTA_USUARIO.append(novo_usuario)
+    USERS.append(novo_usuario)
+
+def sair_sala(msg_dic, client):
+    remove_usuario(msg_dic, client)
+    msg = {
+        "acao": 2,
+        "nome": msg_dic["nome"],
+        "id_sala": msg_dic["id_sala"],
+        "status": 1
+    }
+    msg_json = json.dumps(msg)
+    udp.sendto(msg_json.encode("utf-8"), cliente)
 
 def remove_usuario(usuario, cliente):
     novo_usuario = {}
     novo_usuario["nome"] = usuario["nome"]
     novo_usuario["conexao"] = cliente
     novo_usuario["id_sala"] = usuario["id_sala"]
-    LISTA_USUARIO.remove(novo_usuario)
+    USERS.remove(novo_usuario)
 
-def transmitir_msg(msg, udp):
+
+
+def chat_room(msg_dic, client):
+    msg_chat = {
+        "id_sala": msg_dic["id_sala"],
+        "nome": msg_dic["nome"],
+        "msg": msg_dic["message"] 
+    }
+    CHAT.put(msg_chat)
+    msg = {
+        "acao": 3,
+        "nome": msg_dic["nome"],
+        "id_sala": msg_dic["id_sala"],
+        "id_msg": msg_dic["id_msg"],
+        "status": 1
+    }
     msg_json = json.dumps(msg)
-    for usuario in LISTA_USUARIO:
-        if usuario["id_sala"] == msg["idsala"]:
-            udp.sendto(msg_json.encode("utf-8"), usuario["conexao"])
+    udp.sendto(msg_json.encode("utf-8"), client)
 
-def chat_server(udp):
-    print(f"Starting UDP Server on port {PORT}")
-    orig = ("", PORT)
-    udp.bind(orig)
+def broadcast():
     while True:
-        msg, cliente = udp.recvfrom(1024)
-        msg_decoded = msg.decode('utf-8')
-        try:
-            string_dict = json.loads(msg_decoded)
-            if string_dict["acao"] == 1:
-                adicionar_usuario(string_dict, cliente)
-                msg = {
-                    "acao": 1,
-                    "nome": string_dict["nome"],
-                    "id_sala": string_dict["id_sala"],
-                    "status": 1
-                }
-                msg_json = json.dumps(msg)
-                udp.sendto(msg_json.encode("utf-8"), cliente)
-            elif string_dict["acao"] == 2:
-                remove_usuario(string_dict, cliente)
-                msg = {
-                    "acao": 2,
-                    "nome": string_dict["nome"],
-                    "id_sala": string_dict["id_sala"],
-                    "status": 1
-                }
-                msg_json = json.dumps(msg)
-                udp.sendto(msg_json.encode("utf-8"), cliente)
-            elif string_dict["acao"] == 3:
-                msg = {
-                    "acao": 3,
-                    "nome": string_dict["nome"],
-                    "id_sala": string_dict["id_sala"],
-                    "id_msg": string_dict["id_msg"],
-                    "status": 1
-                }
-                msg_json = json.dumps(msg)
-                udp.sendto(msg_json.encode("utf-8"), cliente)
-                
-                msg_chat = {
-                    "id_sala": string_dict["id_sala"],
-                    "nome": string_dict["nome"],
-                    "msg": string_dict["message"] 
-                }
-                _thread.start_new_thread(transmitir_msg, (msg_chat,udp))
+        while not CHAT.empty():
+            msg_chat = CHAT.get()
+            msg_json = json.dumps(msg_chat)
 
-        except Exception as ex:
-            pass
-    udp.close()
+            for user in USERS:
+                if user["id_sala"] == msg_chat["id_sala"]:
+                    udp.sendto(msg_json.encode("utf-8"), user["conexao"])
 
-def main():
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    chat_server(udp)
 
-if __name__ == "__main__":
-    main()
+
+t1 = threading.Thread(target=receive)
+t2 = threading.Thread(target=controll)
+t3 = threading.Thread(target=broadcast)
+
+t1.start()
+t2.start()
+t3.start()
+
+
